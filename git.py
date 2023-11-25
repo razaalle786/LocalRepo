ccbfc3b79a077b8ac7929d8474a254877baecfb8
razaalle786 <razaalle786@hotmail.com>
2023-11-25 18:33:47 +0000

    Readme.txt file added

--- git.py
+++ git.py
@@ -4,3 +4,4 @@ print("this is a new git repo ")
 # git remote -v (use to verify the origin )
 # to rename a branch use command git branch -M branch name
 # if you dont want to use origing repeatedly in push commands use git push-u origin main
+# now you can only use command git push
