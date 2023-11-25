a80757c64550c35ea311fd43b89e3ec9b8b6ad9b
razaalle786 <razaalle786@hotmail.com>
2023-11-25 18:27:45 +0000

    how to rename and push again

--- git.py
+++ git.py
@@ -2,3 +2,5 @@ print("this is a new git repo ")
 #git remote add origin and then <-link>
 # git remote add origin https://github.com/razaalle786/LocalRepo.git
 # git remote -v (use to verify the origin )
+# to rename a branch use command git branch -M branch name
+# if you dont want to use origing repeatedly in push commands use git push-u main origin
