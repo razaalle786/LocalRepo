480fd9b2532f84f99ac03d42c8371962ae401736
razaalle786 <razaalle786@hotmail.com>
2023-11-25 18:30:04 +0000

    push

--- git.py
+++ git.py
@@ -3,4 +3,4 @@ print("this is a new git repo ")
 # git remote add origin https://github.com/razaalle786/LocalRepo.git
 # git remote -v (use to verify the origin )
 # to rename a branch use command git branch -M branch name
-# if you dont want to use origing repeatedly in push commands use git push-u main origin
+# if you dont want to use origing repeatedly in push commands use git push-u origin main
